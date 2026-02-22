01bfe7ef9e041b0b6d89cf3dc3baa8ade85c7292
over3363 <over3363@gmail.com>
2026-02-21 21:28:14 -0800

    Manual Regression Values

--- LinearRegression/Wineplots.py
+++ LinearRegression/Wineplots.py
@@ -36,4 +36,59 @@ wine_df = pd.read_csv(r'C:\Users\Tori\Documents\MAELecture\LinearRegression\Line
 # when we did data exploration we could see .4 positive correlation of alchol and quality so not a huge one but a potentially existing one
 # function or command explinations 
 
+# Manual Regression Between Alcohol and Quality 
 
+# Get Variables We Need to Make Calculations 
+
+# x variable 
+x = "alcohol"
+# y variable 
+y = "quality"
+
+# n is the number of samples we have total going to say that is going to be the length of the wine dataframe index 
+# index gives you all the row indexs on the dataframe 
+# the length will give you physical number count 
+# n becomes our sample number
+n = len(wine_df.index)
+
+# sum of all x values 
+# sum of x is equal to the wine dataframe x variable which is alcohol and we are goign to call sum
+sum_x = wine_df[x].sum()
+
+# sum of all y values?
+# sum y is equal to the wine dataframe at the quality variable y and again take the sum function of dataframe to do this automatically 
+sum_y = wine_df[y].sum()
+
+# x mean
+# equal to the sum divided by n the number of samples 
+x_mean = sum_x/n
+
+# y mean 
+# equal to the sum divided by n the number of samples 
+y_mean = sum_y/n
+
+# sum_x_times_sum_y long variable name
+# sum_x_times_sum_y is equal to wine dataframe of the x value times the wine dataframe at the y value 
+# that value is we are going to apply the sum function on 
+sum_x_times_sum_y = (wine_df[x]*wine_df[y]).sum()
+
+# sum_x_squareds variable name for sum of x squareds
+# wine dataframe at the x value which is alchohol 
+# and then we use handy math power function with 2 so we are squaring it 
+# then we will take the sum of that
+sum_x_squareds = wine_df[x].pow(2).sum()
+
+# sanity check 
+# print 
+# print the n value of the variable 
+# n is equal to : n value of the variable {n}
+# inefficient way to do this but create a print statment for each variable 
+print(f"n: {n}")
+# sum of x values going to be sum_x 
+# this is an f stream, so if you put f in front of a stream than if you have curly braces {} you can put variables inside it and it will evaluate to their value inside the string
+print(f"Sum of X Values: {sum_x}")
+print(f"Sum of Y Values: {sum_y}")
+print(f"Mean X Value : {x_mean}")
+print(f"Mean Y Value : {y_mean}")
+print(f"Sum of X Times Sum of Y : {sum_x_times_sum_y}")
+print(f"Sum of X Sqaureds: {sum_x_squareds}")
